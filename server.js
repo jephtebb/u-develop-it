80a121571a49d3453ff51da82817b945ba048746
Jephtebb <jephteborno@yahoo.com>
2021-01-20 19:50:58 -0500

    create the delete route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,92 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other requests(Not Found) Catch all
-app.use((req, res) => {
-  res.status(404).end();
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log('Connected to the election database.');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Default response for any other requests(Not Found) Catch all
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
+
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
+});
+
+
+// Create a candidate
+
+// Get all candidates
+
+
+// app.get('/api/candidates', (req, res) => {
+//   const sql = `SELECT * FROM candidates`;
+//   const params = [];
+//   db.all(sql, params, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
+// });
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates 
+               WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Start server after DB connection
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
